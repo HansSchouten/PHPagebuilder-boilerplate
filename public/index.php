5b9185e7d183785d5d054663d1f279448546d3ce
Hans Schouten <hans@falcosolutions.nl>
2019-10-29 08:24:17 +0100

    Config removed

--- public/index.php
+++ public/index.php
@@ -7,8 +7,7 @@ error_reporting(E_ALL);
 //require_once '../vendor/autoload.php';
 
 // Uncomment if you downloaded PHPagebuilder
-//$installationFolder = __DIR__ . '/../phpagebuilder';
-//require_once $installationFolder . '/src/Core/helpers.php';
+//require_once __DIR__ . '/../phpagebuilder/src/Core/helpers.php';
 //spl_autoload_register('phpb_autoload');
 
 $config = require __DIR__ . '/../config.php';
