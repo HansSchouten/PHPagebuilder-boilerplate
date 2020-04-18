ed18f7a5d021c4bcf50fe8eb8eebdf5d44c4c406
Hans Schouten <hans@falcosolutions.nl>
2020-04-18 07:35:13 +0200

    Page title added

--- themes/demo/layouts/master/view.php
+++ themes/demo/layouts/master/view.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title></title>
+    <title><?= e($page->get('title')) ?></title>
 
     <link href="<?= phpb_theme_asset('css/style.css') ?>" rel="stylesheet">
 </head>
